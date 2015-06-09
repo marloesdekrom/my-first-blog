d88a96aba6ace92c357fb65f8452300eb2fa9c87
Marloes <marloes.de.krom@gmail.com>
2015-06-09 19:14:06 +0200

    fixed error

--- blog/views.py
+++ blog/views.py
@@ -18,14 +18,14 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post=form.save(commit=False)
-            post.author=request.user
+            post = form.save(commit=False)
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect ('blog.views.post_detail', pk=post.pk)
-        else:
-            form=PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request,pk):
     post = get_object_or_404(Post, pk=pk)
@@ -39,4 +39,4 @@ def post_edit(request,pk):
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form=PostForm(instance=post)
-        return render (request, 'blog/post_edit.html', {'form': form})            
+        return render (request, 'blog/post_edit.html', {'form': form})
